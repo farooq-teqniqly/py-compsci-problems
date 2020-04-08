cdf8d199727a21841b647e49b3426da17c71907e
Farooq Mahmud <f>
2020-04-07 18:20:51 -1000

    Fibbonacci

--- m1.py
+++ m1.py
@@ -1,6 +1,12 @@
 """
     Chapter 1 problems in the book "Classic Computer Science Problems in Python."
 """
+from typing import Dict, Generator
+from itertools import count
+
+memo: Dict[int, int] = {0: 0, 1: 1}
+
+
 def fibbonacci(n: int) -> int:
     """
         Computes the fibbonacci number for the given input number.
@@ -14,10 +20,28 @@ def fibbonacci(n: int) -> int:
         Raises:
             ValueError: The input number is less than zero.
     """
+
     if n < 0:
         raise ValueError("Number cannot be less than zero.")
 
-    if n in [0, 1]:
-        return n
+    if n not in memo:
+        memo[n] = fibbonacci(n - 1) + fibbonacci(n - 2)
 
-    return fibbonacci(n - 2) + fibbonacci(n - 1)
+    return memo[n]
+
+
+def fibbonacci_sequence() -> Generator[int, None, None]:
+    """
+        Returns the Fibbonacci sequence.
+    Returns:
+        A generator containing the Fibbonacci sequence.
+    """
+    yield 0
+    yield 1
+
+    last: int = 0
+    next: int = 1
+
+    for _ in count(1):
+        last, next = next, last + next
+        yield next
--- tests/test_m1.py
+++ tests/test_m1.py
@@ -1,13 +1,25 @@
 import unittest
-from m1 import fibbonacci
+from typing import List
+from itertools import islice
+
+from m1 import fibbonacci, fibbonacci_sequence
+
 
 class TestFibbonacci(unittest.TestCase):
-    def test_returns_expected_result(self):
+    def test_fibbonacci_returns_expected_result(self):
         # Arrange, Act
         result: int = fibbonacci(35)
 
         # Assert
         self.assertEqual(9227465, result)
+
+    def test_fibbonacci_sequence_returns_expected_result(self):
+        # Arrange, Act
+        result: List[int] = list(islice(fibbonacci_sequence(), 0, 11))
+
+        # Assert
+        self.assertEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55], result)
+
 
 if __name__ == "__main__":
     unittest.main()
